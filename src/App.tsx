0ad5902a8b5bbc5de677d554e9331b021a014def
Alvertis Minas <mnsalbert@gmail.com>
2026-07-28 00:19:50 +0300

    Enable piece movent
    - Click a legal move square to relocate the selected piece
    - Clear selection and possible moves after a successful move

--- src/App.tsx
+++ src/App.tsx
@@ -13,12 +13,24 @@ const App = () => {
   const [possibleMoves, setPossibleMoves] = useState<Coord[] | null>([]);
 
   const handleSquareClick = (row: number, col: number) => {
+    // If the square is the already selected piece, deselect it
     if (selected?.row === row && selected?.col === col) {
       setSelected(null);
       setPossibleMoves(null);
       return;
     }
 
+    // If the square is a possible move, move the selected piece and deselect it
+    if (
+      selected &&
+      possibleMoves &&
+      possibleMoves.some((move) => move.row === row && move.col === col)
+    ) {
+      handleMove({ row, col });
+      return;
+    }
+
+    // if there is no selected piece, select the piece and show possible moves
     const piece = boardState[row][col];
 
     if (piece) {
@@ -27,8 +39,29 @@ const App = () => {
       return;
     }
 
+    // if the square is empty, deselect the piece
     setSelected(null);
     setPossibleMoves(null);
+  };
+
+  const handleMove = (destination: Coord) => {
+    if (selected && possibleMoves && destination) {
+      setBoardState(movePiece(boardState, selected, destination));
+      setSelected(null);
+      setPossibleMoves(null);
+    }
+  };
+
+  const movePiece = (
+    boardState: BoardState,
+    selected: Coord,
+    destination: Coord,
+  ) => {
+    const newBoardState = [...boardState];
+    newBoardState[destination.row][destination.col] =
+      boardState[selected.row][selected.col];
+    newBoardState[selected.row][selected.col] = null;
+    return newBoardState;
   };
 
   return (
